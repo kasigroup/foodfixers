f53ec87690ce915f745fde91b6871e5099d4e7bb
isakfagerlund <fagerlund.isak@gmail.com>
2017-01-25 15:55:30 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# foodfixers_api
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './css/style.css';
+import App from './components/App';
+
+render(<App />, document.querySelector("#main"));
