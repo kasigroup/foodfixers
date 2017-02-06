f41fc3b45ce1f9c6965b5cc115ee7d67f0c9887d
isakfagerlund <fagerlund.isak@gmail.com>
2017-02-06 12:10:38 +0100

    Redux Start

---
+++ client/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Foodfixers
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default Main;
