7490f9357997a2b0d58d63fbd0e4af76dc441d51
Isak Fagerlund <fagerlund.isak@gmail.comgit config --global user.email fagerlund.isak@gmail.com>
2017-05-04 20:04:46 +0200

    Transition added to app

--- client/containers/App.js
+++ client/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import NavBar from './NavBar';
+import { RouteTransition } from 'react-router-transition';
 
 
 
@@ -11,7 +12,14 @@ class App extends Component{
         <div className="top-divider"></div>
         <div id="main">
           <h4 className="logo"></h4>
-          {this.props.children}
+          <RouteTransition
+            pathname={this.props.location.pathname}
+            atEnter={{ opacity: 0 }}
+            atLeave={{ opacity: 0 }}
+            atActive={{ opacity: 1 }}
+          >
+            {this.props.children}
+          </RouteTransition>
         </div>
       </div>
     )
