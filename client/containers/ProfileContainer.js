30738b467e2dc666b65e293aeffed9e3d8c6b691
Isak Fagerlund <fagerlund.isak@gmail.comgit config --global user.email fagerlund.isak@gmail.com>
2017-05-23 15:41:11 +0200

    Loading on profile page

--- client/containers/ProfileContainer.js
+++ client/containers/ProfileContainer.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { loadProfile,loadOrders } from '../actions/profileActions'
 import { getVisibleProducts } from '../reducers/products'
-
+import Loader from 'react-loader-advanced';
 
 class ProfileContainer extends Component {
   constructor(props) {
@@ -18,12 +18,22 @@ class ProfileContainer extends Component {
 
    render() {
      const { profile, orders, products } = this.props
+
+     function Orders() {
+       if (orders.length <= 0) {
+         return <p>Loading...</p>;
+       }else {
+         return <div>{orders.map((item, i) =>  <div key={i}><p>id: {item.id} Price: {item.total}kr</p></div>)}</div>;
+       }
+
+     }
+
      return (
        <div>
          <h3 className="title">Profile</h3>
          <p className="profile-name">{profile.first_name} <span/> {profile.last_name}</p>
          <h3>Orders</h3>
-         {orders.map((item, i) =>  <div key={i}><p>id: {item.id} Price: {item.total}kr</p></div>)}
+         <Orders />
        </div>
      )
    }
