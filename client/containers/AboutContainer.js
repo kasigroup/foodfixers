ffb6b23ad023f7c4222624f643c0f3f8232a90fa
Isak Fagerlund <fagerlund.isak@gmail.com>
2017-09-04 17:52:20 +0200

    Update AboutContainer.js

--- client/containers/AboutContainer.js
+++ client/containers/AboutContainer.js
@@ -19,26 +19,26 @@ class AboutContainer extends Component {
 
           <p>Vi är en sushibar i din mobil. Idén är att samla beställningar en dag i förväg genom appen från våra kunder och leverera maten till fasta platser på bestämda tider enligt ett veckoschema.</p>
 
-<p><b>Det finns många fördelar med foodfixers.se:</b></p>
+          <p><b>Det finns många fördelar med foodfixers.se:</b></p>
 
-<p>-Du slipper stressa iväg på lunchen för att hämta mat långt bort.</p>
+          <p>-Du slipper stressa iväg på lunchen för att hämta mat långt bort.</p>
 
-<p>-Du vet att vi kommer en viss dag en viss tid varje vecka och det kan bli din sushidag.</p>
+          <p>-Du vet att vi kommer en viss dag en viss tid varje vecka och det kan bli din sushidag.</p>
 
-<p>-Vi vet precis hur mycket mat som kommer gå åt och slipper matsvinn.</p>
+          <p>-Vi vet precis hur mycket mat som kommer gå åt och slipper matsvinn.</p>
 
-<p>-Eftersom vi kör ut mat till många människor i samma leverans blir det mindre trafik än vid traditionell utkörning där man kör kors och tvärs med en och en portion.</p>
+          <p>-Eftersom vi kör ut mat till många människor i samma leverans blir det mindre trafik än vid traditionell utkörning där man kör kors och tvärs med en och en portion.</p>
 
-<p>-Vi tar ingen avgift för utkörningen.</p>
+          <p>-Vi tar ingen avgift för utkörningen.</p>
 
-<p>-Vi tillagar din sushi precis före leverans.</p>
+          <p>-Vi tillagar din sushi precis före leverans.</p>
 
-<p>-Ingen samordning behövs på jobbet, var och en beställer och betalar för sig själv.</p>
+          <p>-Ingen samordning behövs på jobbet, var och en beställer och betalar för sig själv.</p>
 
-<p>-Alla plaster vi levererar till ligger vid stora företag med gott om parkering. Chansen är stor att vi levererar till ditt jobb, eller precis i närheten där det är lätt att hämta.</p>
+          <p>-Alla plaster vi levererar till ligger vid stora företag med gott om parkering. Chansen är stor att vi levererar till ditt jobb, eller precis i närheten där det är lätt att hämta.</p>
 
-<p>-Du kan hämta i vårt produktionskök på Hedenstorp om det är närmare för dig, ring i så fall och förbeställ och betala sedan med Swhish.
-</p>
+          <p>-Du kan hämta i vårt produktionskök på Hedenstorp om det är närmare för dig, ring i så fall och förbeställ och betala sedan med Swhish.
+          </p>
 
           <div className="contact">
             <p><b>Kontaktuppgifter</b></p>
